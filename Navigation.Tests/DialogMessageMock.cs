0c0b5695803937e18eed33a2d33ed447e1a9400d
Stuart Bowes <stuartbowes2000@gmail.com>
2021-07-08 10:21:53 +0100

    Add test project and MainViewModel test

---
+++ Navigation.Tests/DialogMessageMock.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+
+namespace Navigation.Tests
+{
+    class DialogMessageMock : IDialogMessage
+    {
+        public int DisplayAlertCallCount { get; set; }
+
+        public Task DisplayAlert(string title, string message, string cancel)
+        {
+            DisplayAlertCallCount++;
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Navigation.Tests/MainViewModelTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Navigation.Tests
+{
+    public class MainViewModelTests
+    {
+        [Fact]
+        public void DisplayAlertCommand_displays_alert()
+        {
+            var dialogMessage = new DialogMessageMock();
+            var viewModel = new MainViewModel(dialogMessage);
+
+            viewModel.DisplayAlertCommand.Execute(null);
+
+            Assert.Equal(1, dialogMessage.DisplayAlertCallCount);
+        }
+    }
+}
